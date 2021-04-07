804a74b758b731606f9092fc38dff6e9a147731e
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-04-07 12:06:42 +0100

    feat: Add module for Botometer functions.

---
+++ src/modules/botometer.py
@@ -0,0 +1,10 @@
+"""Module containing Botometer functions for the application.
+"""
+import botometer
+
+# Local import
+import modules.helpers as helpers
+
+# TODO: Function to authenticate to Botometer API
+
+# TODO: Function to get Twitter friend bot likelihood score
