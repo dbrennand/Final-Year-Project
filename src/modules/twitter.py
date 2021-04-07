46177d216c0d68a9246427127f4fa10f19159aca
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-04-07 12:06:53 +0100

    feat: Add module for Twitter functions.

---
+++ src/modules/twitter.py
@@ -0,0 +1,10 @@
+"""Module containing Twitter functions for the application.
+"""
+import tweepy
+
+# Local import
+import modules.helpers as helpers
+
+# TODO: Function to authenticate to Twitter API
+
+# TODO: Function to get Twitter friends
