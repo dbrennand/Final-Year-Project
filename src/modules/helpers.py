9eaaa476f79b993c158cec192fb9fe23cba9581c
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-04-08 16:50:58 +0100

    feat: Add `get_env_vars` function.
    Added to prevent repetition of os.environ.get.

--- src/modules/helpers.py
+++ src/modules/helpers.py
@@ -33,6 +33,41 @@ def log_message(message: str, level: str = "INFO") -> None:
 # Utility functions
 
 
+def get_env_vars(env_vars: list) -> dict:
+    """Get a dictionary containing the provided environment variable(s) name(s) and value(s).
+
+    Args:
+        env_vars (list): A list of environment variable name(s) to retrieve the value(s) for.
+
+    Raises:
+        ValueError: Occurs when an environment variable value is None.
+
+    Returns:
+        dict: A dictionary containing the environment variable name(s) and value(s) as key value pairs.
+    """
+    # Initialise environment variable dictionary
+    env_vars_dict = {}
+    for env_var in env_vars:
+        # Get environment variable value
+        # Default to None if not found
+        env_var_value = os.environ.get(env_var, None)
+        # Check env_var_value is not None
+        # If None, log error and raise ValueError
+        if isinstance(env_var_value, None):
+            log_message(
+                message=f"Environment variable: {env_var} is missing. See prerequisite steps in the README file.",
+                level="ERROR",
+            )
+            raise ValueError(
+                f"Environment variable: {env_var} is missing. See prerequisite steps in the README file."
+            )
+        # Is present as a str
+        else:
+            # Add environment variable name and value to env_vars_dict
+            env_vars_dict[env_var] = env_var_value
+    return env_vars_dict
+
+
 def get_api_creds() -> typing.Tuple[dict, str]:
     """Get API credentials from environment variables.
 
@@ -68,3 +103,19 @@ def get_api_creds() -> typing.Tuple[dict, str]:
         "consumer_secret": twitter_api_secret,
     }
     return twitter_auth, botometer_api_key
+
+
+def get_email_creds() -> typing.Tuple[str, str, int]:
+    """Get email server credentials from environment variables.
+
+    Raises:
+        ValueError: Occurs when one or more email server credentials are missing.
+
+    Returns:
+        typing.Tuple[str, str, int]: A tuple containing two strings amd an integer.
+            String 1: The email server domain name.
+            String 2: The email server password.
+            Integer: The email server port used to connect to the email server.
+    """
+    # Get email credentials from environment variables
+    pass
