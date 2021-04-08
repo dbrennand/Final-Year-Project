eb7fa101a4160605de0b28b636b4c470764647cc
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-04-08 18:15:15 +0100

    feat: Add auth function.
    Add authentication function for the Botometer API.

--- src/modules/botm.py
+++ src/modules/botm.py
@@ -5,6 +5,24 @@ import botometer
 # Local import
 import modules.helpers as helpers
 
-# TODO: Function to authenticate to Botometer API
+
+def auth(botometer_api_creds: dict) -> botometer.Botometer:
+    """Authenticate to the Botometer API using botometer-python.
+
+    Args:
+        botometer_api_creds (dict): A dictionary containing the required credentials for the Botometer constructor
+            to authenticate to the Botometer and Twitter API.
+
+    Returns:
+        botometer.Botometer: A botometer.Botometer object authenticated to the Botometer and Twitter API.
+    """
+    # Initialise botometer constructor and provide API credentials
+    # Provide the same Tweepy parameters to the constructor
+    return botometer.Botometer(
+        **botometer_api_creds,
+        tweepy_kwargs={"retry_count": 2, "retry_delay": 3},
+        wait_on_ratelimit=True
+    )
+
 
 # TODO: Function to get Twitter friend bot likelihood score
