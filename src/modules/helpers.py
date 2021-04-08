ce9a9d1859530a2342014d61a796ae964cacbc47
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-04-08 09:31:21 +0100

    feat(utility): Add `get_api_creds` function.
    This function obtains API credentials from environment variables.

--- src/modules/helpers.py
+++ src/modules/helpers.py
@@ -1,6 +1,8 @@
 """Module containing helper functions for the application.
 """
 import loguru
+import os
+import typing
 
 # Logging functions
 
@@ -26,3 +28,43 @@ def log_message(message: str, level: str = "INFO") -> None:
         None.
     """
     loguru.logger.log(level, message)
+
+
+# Utility functions
+
+
+def get_api_creds() -> typing.Tuple[dict, str]:
+    """Get API credentials from environment variables.
+
+    Raises:
+        ValueError: Occurs when one or more API credentials are missing.
+
+    Returns:
+        typing.Tuple[dict, str]: A tuple containing a dictionary and a string.
+            Dictionary: Contains credentials for the Tweepy constructor.
+            String: Contains the Botometer API key.
+    """
+    # Obtain API credentials from environment variables
+    # Twitter API credentials
+    twitter_api_key = os.environ.get("TWITTER_API_KEY", None)
+    titter_api_secret = os.environ.get("TWITTER_API_SECRET", None)
+    # Botometer (Rapid) API key
+    botometer_api_key = os.environ.get("BOTOMETER_API_KEY", None)
+    # Check API credentials are not None
+    # If None, log error and raise ValueError
+    if not all([twitter_api_key, twitter_api_secret, botometer_api_key]):
+        log_message(
+            message="One or more API credentials are missing. Provide the environment variables: TWITTER_API_KEY, TWITTER_API_SECRET and BOTOMETER_API_KEY.",
+            level="ERROR",
+        )
+        raise ValueError(
+            "One or more API credentials are missing. See usage heading in the README file."
+        )
+    # Return a tuple containing a dictionary and string
+    # Dictionary contains credentials for the Tweepy constructor and the string is the Botometer API key
+    # Only returning the Botometer API key because Botometer also requires the Twitter API credentials in its constructor
+    twitter_auth = {
+        "consumer_key": twitter_api_key,
+        "consumer_secret": twitter_api_secret,
+    }
+    return twitter_auth, botometer_api_key
