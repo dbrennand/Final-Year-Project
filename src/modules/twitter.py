ca5a3b8281a1c74541eaf864cf9ac1942fa4b40a
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-04-08 17:49:09 +0100

    refactor: Change auth function parameter.
    From twitter_creds to twitter_api_creds.

--- src/modules/twitter.py
+++ src/modules/twitter.py
@@ -6,20 +6,20 @@ import tweepy
 import modules.helpers as helpers
 
 
-def auth(twitter_creds: dict) -> tweepy.API:
+def auth(twitter_api_creds: dict) -> tweepy.API:
     """Authenticate to the Twitter API using Tweepy.
 
     https://docs.tweepy.org/en/latest/api.html#tweepy.API
 
     Args:
-        twitter_creds (dict): A dictionary containing the Twitter application API credentials.
+        twitter_api_creds (dict): A dictionary containing the Twitter application API credentials.
             Credentials are the API key and secret (also known as consumer key and secret respectively).
 
     Returns:
         tweepy.API: A Tweepy API object authenticated to the Twitter API.
     """
     # Initialise Tweepy application authentication flow
-    auth = tweepy.AppAuthHandler(**twitter_creds)
+    auth = tweepy.AppAuthHandler(**twitter_api_creds)
     # Create and return an authenticated Tweepy API object
     # Retry 2 times if a request fails with a 3 second delay between retries
     # Wait if the application hits the Twitter API rate limit
